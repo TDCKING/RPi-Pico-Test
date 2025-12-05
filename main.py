bc2a939d6543971ffb77c72ca104a49298a5f933
TDCKING <61333409+TDCKING@users.noreply.github.com>
2025-12-04 22:53:27 -0600

    Template for NeoPixels

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+import neopixel
 import select
 import sys
 import machine
 import time
  
 MAX_BUFFER = 16 #max buffer for reading timer values
+BRIGHTNESS_FACTOR = 0.02
+
+np = neopixel.NeoPixel(machine.Pin(4), 64)
+
+# Set colors for individual LEDs
+np[0] = (255, 0, 0) # Red
+np[1] = (0, 128, 0) # Green (half brightness)
+np[2] = (0, 0, 64) # Blue (quarter brightness)
 
 led = machine.Pin(25, machine.Pin.OUT) #controls LED
 pollObj = select.poll() #instantiate a poll object
@@ -25,13 +34,52 @@ def blinkLed():
     #ledBlink = not ledBlink
     #led.value(0)
     #time.sleep(1)
+
+import time
+
+def demo(np):
+    n = np.n
+
+    # Cycle animation
+    for i in range(4 * n):
+        for j in range(n):
+            np[j] = (0, 0, 0)
+        np[i % n] = (int(255*BRIGHTNESS_FACTOR), int(255*BRIGHTNESS_FACTOR), int(255*BRIGHTNESS_FACTOR))
+        np.write()
+        time.sleep_ms(25)
+
+    # Bounce animation
+    for i in range(4 * n):
+        for j in range(n):
+            np[j] = (0, 0, int(128*BRIGHTNESS_FACTOR))
+        if (i // n) % 2 == 0:
+            np[i % n] = (0, 0, 0)
+        else:
+            np[n - 1 - (i % n)] = (0, 0, 0)
+        np.write()
+        time.sleep_ms(60)
+
+    # Fade in/out animation
+    for i in range(0, 4 * 256, 8):
+        for j in range(n):
+            val = i & 0xff if (i // 256) % 2 == 0 else 255 - (i & 0xff)
+            np[j] = (int(val*BRIGHTNESS_FACTOR), 0, 0)
+        np.write()
+
+    # Clear LEDs
+    for i in range(n):
+        np[i] = (0, 0, 0)
+    np.write()
  
 while True:
     #Check for data available on stdin
-    if pollObj.poll(0):
-        num = sys.stdin.read(1) #read one character from stdin
-        if num == 'p':
-            print("Received: " + num)
-            blinkLed()
+    #if pollObj.poll(0):
+    #    num = sys.stdin.read(1) #read one character from stdin
+    #    if num == 'p':
+     #       print("Received: " + num)
+     #       blinkLed()
 
-    time.sleep(0.1)
+    demo(np)
+    
+
+   # time.sleep(0.1)
